86bb7d1a680556987d3c112c2d34174fee19109d
Jocer <jiguanhu@wepie.com>
2023-11-30 20:30:49 +0800

    feat: add some extensions

--- project_static_analysis.py
+++ project_static_analysis.py
@@ -42,6 +42,7 @@ additional_resource_folders = None
 disable_gitignore = None
 apply_filtered_files = None
 analyze_resources = None
+exclude_resource_folders = None
 
 def getch():
     fd = sys.stdin.fileno()
@@ -75,15 +76,15 @@ def filter_files_by_type(all_files, filter_file_types):
 def find_gitignore():
     for root, dirs, files in os.walk(input_path):
         if '.gitignore' in files:
-            return os.path.join(root, '.gitignore')
+            ignore_path = os.path.join(root, '.gitignore')
+            print(get_colored__description_and_object("检索到.gitignore文件:", ignore_path))
+            return ignore_path
     return None
 
 def filter_files_by_gitignore(all_files):
-    print(get_colored__description_and_object("正在应用.gitignore规则过滤文件..."))
     gitignore_file = find_gitignore()
     ignored_patterns = []
     if os.path.exists(gitignore_file):
-        print("检索到.gitignore文件:", gitignore_file)
         with open(gitignore_file, "r") as f:
             ignored_patterns = f.read().splitlines()
             print(*ignored_patterns, sep="\n")
@@ -153,51 +154,96 @@ def apply_classes_json(filtered_files):
 
 def apply_resources():
     print(get_colored__description_and_object("正在检索项目中的资源信息..."))
-    def extract_imageset_name(path):
-        # 提取imageset名称
-        imageset_path = os.path.dirname(path)
-        imageset_name = os.path.basename(imageset_path)
-        if imageset_name.endswith('.imageset'):
-            imageset_name = imageset_name[:-9]
-        return imageset_name
-
-    imagesets = []
-    others = {} 
-
+    RED = "\033[91m"
+    BLUE = "\033[94m"
+    RESET = "\033[0m"
+    def find_matching_folders(base_path, folders):
+        if not folders:
+            return None
+        matching_folders = set()
+        for folder in folders:
+            for root, dirs, files in os.walk(base_path):
+                if folder in dirs:
+                    matching_folders.add(os.path.join(root, folder))
+        return list(matching_folders)
     # 检索imageset
-    imageset_files = glob.glob(os.path.join(input_path, "**/*.imageset/**/*.*"), recursive=True)
-    for imageset_file in imageset_files:
-        imageset_name = extract_imageset_name(imageset_file)
-        imagesets.append(imageset_name)
-    deduplicated_imagesets = list(set(imagesets))
-    # 在 input_path 下检索 additional_resource_folders 中所有文件夹内的资源文件
-    resource_files = []
-    for folder_name in additional_resource_folders:
-        folder_path = os.path.join(input_path, folder_name)
-        # 如果连接后的路径是一个文件而不是目录，获取其所在的目录
-        if not os.path.isdir(folder_path):
-            folder_path = os.path.dirname(folder_path)
-        # 检索文件夹内的所有资源文件
-        folder_files = glob.glob(os.path.join(folder_path, "**/*.*"), recursive=True)
-        folder_files = [file for file in folder_files if os.path.isfile(file)]
-        resource_files.extend(folder_files)
-    def remove_duplicate_suffix(name):
-        if name.endswith('@2x'):
-            return name[:-3]
-        elif name.endswith('@3x'):
-            return name[:-3]
-        return name
-    for resource_file in resource_files:
-        full_name = os.path.basename(resource_file)
-        name, format = full_name.rsplit(".", 1)
-        name = remove_duplicate_suffix(name)
-        if format in others:
-            others[format].append(name)
-        else:
-            others[format] = [name]
+    def apply_imageset(imageset_files, pbar):
+        def extract_imageset_name(path):
+            imageset_path = os.path.dirname(path)
+            imageset_name = os.path.basename(imageset_path)
+            if imageset_name.endswith('.imageset'):
+                imageset_name = imageset_name[:-9]
+            return imageset_name
 
-    for format, names_list in others.items():
-         others[format] = list(set(names_list))
+        imagesets = []
+        imageset_files = glob.glob(os.path.join(input_path, "**/*.imageset/**/*.*"), recursive=True)
+        for imageset_file in imageset_files:
+            imageset_name = extract_imageset_name(imageset_file)
+            imagesets.append(imageset_name)
+            pbar.write(f"已检索到文件：{BLUE}{imageset_name}{RESET}")
+            pbar.update(1)
+        return list(set(imagesets))
+
+    # 在 input_path 下检索 additional_resource_folders 中所有文件夹内的资源文件
+    def apply_addtional_resouces(additional_resource_files, pbar):
+        def remove_duplicate_suffix(name):
+            if name.endswith('@2x'):
+                return name[:-3]
+            elif name.endswith('@3x'):
+                return name[:-3]
+            return name
+
+        resource_files = set()
+        exclude_resource_paths = find_matching_folders(input_path, exclude_resource_folders)
+        for file_path in additional_resource_files:
+            exclude = False
+            file_name = os.path.basename(file_path)
+            if exclude_resource_paths:
+                is_under_exclude_path = any(
+                    os.path.commonpath([file_path, exclude_path]) == exclude_path
+                    for exclude_path in exclude_resource_paths
+                )
+                if is_under_exclude_path:
+                    pbar.write(f"已排除资源文件：{RED}{file_name}{RESET}")
+                    exclude = True
+            if not exclude:
+                pbar.write(f"已检索资源文件：{BLUE}{file_name}{RESET}")
+                resource_files.add(file_path)
+            pbar.update(1)
+  
+        result = {}
+        for resource_file in resource_files:
+            full_name = os.path.basename(resource_file)
+            name, format = full_name.rsplit(".", 1)
+            name = remove_duplicate_suffix(name)
+            if format in result:
+                result[format].append(name)
+            else:
+                result[format] = [name]
+
+        for format, names_list in result.items():
+            result[format] = list(set(names_list))
+        pbar.update(1)
+        return result
+        
+    imageset_files = glob.glob(os.path.join(input_path, "**/*.imageset/**/*.*"), recursive=True)
+    additional_resource_files = list()
+    additional_resource_folder_paths = find_matching_folders(input_path, additional_resource_folders)
+    for path in additional_resource_folder_paths:
+        if os.path.isdir(path):
+            directory_path = path
+        else:
+            directory_path = os.path.dirname(path)
+        folder_files = glob.glob(os.path.join(directory_path, "**/*.*"), recursive=True)
+        folder_files = [file for file in folder_files if os.path.isfile(file)]
+        additional_resource_files.extend(folder_files)
+
+    with tqdm(total=len(imageset_files) + len(additional_resource_files) + 1, desc=f"检索资源文件", unit="asset", leave=False, bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}, {rate_fmt}]') as pbar:
+        pbar.set_description(get_colored__description_and_object(f"正在检索Assets.xcassets"))
+        deduplicated_imagesets = apply_imageset(imageset_files, pbar)
+        pbar.set_description(get_colored__description_and_object(f"正在检索扩展资源：{additional_resource_folders}"))
+        others = apply_addtional_resouces(additional_resource_files, pbar)
+
     resources = {
         "imagesets": deduplicated_imagesets,
         "others": others
@@ -242,32 +288,42 @@ def apply_dev_files(all_files):
     print(get_colored__description_and_object("已保存提取的类至:",  filtered_classes_path))
     return filtered_files
 
-def contains_digit(input_string):
-    pattern = r'\d'  # 正则表达式模式，匹配任意数字字符
-    match = re.search(pattern, input_string)
-    return match is not None
-
 def check_resource_usage(resource, all_files, pbar, count_unuse, lock):
+
+    def contains_digit(input_string):
+        pattern = r'\d'
+        match = re.search(pattern, input_string)
+        return match is not None
+
+    def is_content_matching_resource(resource, file_content):
+        splited_string_arr = re.compile(r'(\D+|\d+)').findall(resource)
+        filtered_array = [s for s in splited_string_arr if not s.isdigit()]
+        return all(s in content for s in filtered_array)
+
     RED = "\033[91m"
     BLUE = "\033[94m"
     RESET = "\033[0m"
-    if contains_digit(resource): # TODO: 暂时忽略掉带有数字的资源
-        return resource
-    resource_used = False  # 添加一个标记来表示资源是否已经被使用
+    resource_used = False
     pbar.set_description(f"正在检索资源{BLUE}{resource}{RESET}[已检测到的未使用资源数量:{RED}{count_unuse}{RESET}]")
     for file_path in all_files:
         with open(file_path, "r", encoding="utf-8") as file:
             content = file.read()
-            if resource in content:
-                resource_used = True  # 标记资源已经被使用
-                break  # 终止循环
+            if content:
+                t = f"\"{resource}\""
+                if t in content:
+                    resource_used = True
+                    break
+                elif contains_digit(resource) and is_content_matching_resource(resource, content):
+                    resource_used = True
+                    break
+
     if resource_used:
-        return resource  # 返回资源，表示已使用
+        return resource
     else:
         with lock:
             count_unuse[0] += 1
         pbar.write(f"{BLUE}检测到未使用的资源：{RESET}{RED}{resource}{RESET}")
-        return None  # 返回 None，表示未使用
+        return None
 
 def fetch_unused_resources(all_files, resources):
     print(get_colored__description_and_object("正在检索项目中未使用到的资源文件..."))
@@ -292,6 +348,7 @@ def fetch_unused_resources(all_files, resources):
                 if r:
                     resources_to_remove.add(r)
                 pbar.update(1)
+
     executor.shutdown(wait=True)
     unused_resources -= resources_to_remove
     unused_resources = list(unused_resources)
@@ -432,26 +489,35 @@ def create_arg_parser():
             "   Adds 'Resources' and all subdirectories matching the 'CustomAssets' pattern to the resource detection process. Use this option to broaden the scope of resource analysis beyond the default 'Assets.xcassets'."
     )
 
+    # 排除的资源检测，通过该指令排除一个或多个文件夹
+    parser.add_argument(
+        "-erp",
+        "--exclude_resource_folders",
+        nargs='+',
+        help="Exclude specified folders when detecting resource files in the project. "
+            "Use this option to ignore specific directories containing resources that "
+            "should not be processed. Provide a space-separated list of folder names. "
+            "Example: -arp folder1 folder2"
+    )
     return parser
 
 def recreate_output_directory():
-    # 删除目录及其内容
     if os.path.exists(output_path):
         shutil.rmtree(output_path)
 
-    # 创建新目录
     os.makedirs(output_path)
 
 def inititalize_global_variable(args):
-    global input_path, output_path, disable_gitignore, apply_filtered_files, analyze_resources, additional_resource_folders, additional_exclude_file_folder
+    global input_path, output_path, disable_gitignore, apply_filtered_files, analyze_resources, additional_resource_folders, additional_exclude_file_folder, exclude_resource_folders
     input_path = args.input_path
     output_path = args.output_path
-    recreate_output_directory()
     apply_filtered_files = args.apply_filtered_files
     disable_gitignore = args.disable_gitignore
     analyze_resources = args.analyze_resources
     additional_resource_folders = args.additional_resource_folders
     additional_exclude_file_folder = args.additional_exclude_file_folder
+    exclude_resource_folders = args.exclude_resource_folders
+    # recreate_output_directory()
 
 if __name__ == "__main__":
     arg_parser = create_arg_parser()
